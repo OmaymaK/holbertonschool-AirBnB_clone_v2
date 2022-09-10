d617b1af5041d604fb81b9167ff7f6df099a4209
OmaymaK <emmablink050@gmail.com>
2022-09-10 18:37:00 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Flask Web App."""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Hello hbnb."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Hbnb."""
+    return "HBNB"
+
+
+@app.route("/c/<string:text>", strict_slashes=False)
+def cisfun(text):
+    """Cisfun."""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def disispython(text="is cool"):
+    """Python is cool."""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
